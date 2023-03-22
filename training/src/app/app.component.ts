b5fa093ab041a0db394a145ca31c5d7a5363a2fc
Michelle Madeira <michellemadeira1@gmail.com>
2023-03-22 16:16:02 -0300

    login-tela

--- training/src/app/app.component.ts
+++ training/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './pages/login/login.component.html',
+  styleUrls: ['./pages/login/login.component.scss']
 })
 export class AppComponent {
   title = 'training';
--- training/src/app/app.module.ts
+++ training/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CadastroComponent,
+    HomeComponent,
+
   ],
   imports: [
     BrowserModule,
